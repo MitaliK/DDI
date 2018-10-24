3e4a8d2cc0dcf8280c4618058f4adb32e4568da9
Darshan Prakash <darshan@darshans-mbp.3521.dhcp.asu.edu>
2018-10-24 15:04:33 -0700

    adding function for calculating f1 score

--- ddi.py
+++ ddi.py
@@ -1,25 +1,26 @@
-import xml
-import xml.etree.ElementTree as ET
-import nltk
 import csv
 import os
-import numpy as np
+import xml.etree.ElementTree as ET
+
 # Check how long sentences are so that we can pad them
 import matplotlib.pyplot as plt
+import nltk
+import numpy as np
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from keras.models import Model, Input
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from keras.models import Model, Input
-from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
 from seqeval.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 csv_list = []
 file_counter = 0
 sentence_list = []
-word_list =  []
+word_list = []
 tag_list = []
 
-path = '/Users/mitalikulkarni/Documents/Sem_III/BMI 598/Project Document/DDI Data/trainingFiles/'
+# path = '/Users/mitalikulkarni/Documents/Sem_III/BMI 598/Project Document/DDI Data/trainingFiles/'
+path = '/Users/darshan/Documents/github/DDI/trainingFiles/'
 for filename in os.listdir(path):
     if not filename.endswith('.xml'): continue
     fullname = os.path.join(path, filename)
@@ -31,18 +32,18 @@ for filename in os.listdir(path):
             sentences = child
             break
 
-    for sentence_ind,sentence in enumerate(sentences.findall('Sentence')):
+    for sentence_ind, sentence in enumerate(sentences.findall('Sentence')):
         sentenceText = sentence.find('SentenceText').text
         # print sentenceText
         # print nltk.pos_tag(nltk.word_tokenize(sentenceText))
-        tag_dict ={}
+        tag_dict = {}
         for mention in sentence.findall('Mention'):
             for mention_words in mention.attrib['str'].split('|'):
-                for ind,mention_word in enumerate(mention_words.split()):
+                for ind, mention_word in enumerate(mention_words.split()):
                     if not ind:
-                        tag_dict[mention_word] ='B-'+mention.attrib['type']
+                        tag_dict[mention_word] = 'B-' + mention.attrib['type']
                     else:
-                        tag_dict[mention_word] ='I-'+mention.attrib['type']
+                        tag_dict[mention_word] = 'I-' + mention.attrib['type']
         # print tag_dict
 
         temp_list = []
@@ -52,21 +53,21 @@ for filename in os.listdir(path):
             pos = word_tag[1]
             if word in tag_dict:
                 tag = tag_dict[word]
-            csv_list.append([file_counter,sentence_ind, word, pos, tag])
+            csv_list.append([file_counter, sentence_ind, word, pos, tag])
             word_list.append(word)
             tag_list.append(tag)
             temp_list.append((word, pos, tag))
         sentence_list.append(temp_list)
 
-    #for word in sentenceText.text:
-with open('data.csv','w') as fileObj:
+    # for word in sentenceText.text:
+with open('data.csv', 'w') as fileObj:
     wr = csv.writer(fileObj, quoting=csv.QUOTE_ALL)
-    wr.writerow(['filenumber','sentence_idx','word', 'pos', 'tag'])
+    wr.writerow(['filenumber', 'sentence_idx', 'word', 'pos', 'tag'])
     for row in csv_list:
         wr.writerow(row)
 
 maxlen = max([len(s) for s in sentence_list])
-print ('Maximum sequence length:', maxlen)
+print('Maximum sequence length:', maxlen)
 
 # sentence plot
 # plt.style.use("ggplot")
@@ -84,14 +85,13 @@ word2idx = {w: i for i, w in enumerate(words)}
 tag2idx = {t: i for i, t in enumerate(tags)}
 
 X = [[word2idx[w[0]] for w in s] for s in sentence_list]
-#pad with ENDPAD
-X = pad_sequences(maxlen=100, sequences=X, padding="post",value=n_words - 1)
-
+# pad with ENDPAD
+X = pad_sequences(maxlen=100, sequences=X, padding="post", value=n_words - 1)
 
 Y = [[tag2idx[w[2]] for w in s] for s in sentence_list]
-#pad with 2 which is index of capital o
+# pad with 2 which is index of capital o
 Y = pad_sequences(maxlen=100, sequences=Y, padding="post", value=tag2idx["O"])
-#converted to one hot vector
+# converted to one hot vector
 Y = [to_categorical(i, num_classes=n_tags) for i in Y]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
@@ -99,7 +99,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 input = Input(shape=(100,))
 model = Embedding(input_dim=n_words, output_dim=100, input_length=100)(input)
 model = Dropout(0.1)(model)
-model = Bidirectional(LSTM(units=100, return_sequences=True, recurrent_dropout=0.1))(model)# variational biLSTM
+model = Bidirectional(LSTM(units=100, return_sequences=True, recurrent_dropout=0.1))(model)  # variational biLSTM
 out = TimeDistributed(Dense(n_tags, activation="softmax"))(model)  # softmax output layer
 model = Model(input, out)
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
@@ -109,8 +109,8 @@ i = 0
 p = model.predict(np.array([X_test[i]]))
 p = np.argmax(p, axis=-1)
 print("{:15} ({:5}): {}".format("Word", "True", "Pred"))
-for w,pred in zip(X_test[i],p[0]):
-    print("{:15}: {}".format(words[w],tags[pred]))
+for w, pred in zip(X_test[i], p[0]):
+    print("{:15}: {}".format(words[w], tags[pred]))
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -132,6 +132,19 @@ plt.show()
 
 test_pred = model.predict(X_test, verbose=1)
 idx2tag = {i: w for w, i in tag2idx.items()}
-pred_labels = pred2label(test_pred, idx2tag)
-test_labels = pred2label(Y_test, idx2tag)
+
+
+def pred2label(pred):
+    out = []
+    for pred_i in pred:
+        out_i = []
+        for p in pred_i:
+            p_i = np.argmax(p)
+            out_i.append(idx2tag[p_i].replace("PAD", "O"))
+        out.append(out_i)
+    return out
+
+
+pred_labels = pred2label(test_pred)
+test_labels = pred2label(Y_test)
 print(classification_report(test_labels, pred_labels))
