01d99d2aaa5acb95f5bfa4a1fe0e1e4ae6241870
Darshan Prakash <darshan@darshans-mbp.3521.dhcp.asu.edu>
2018-11-25 12:19:44 -0700

    updateing ddi

--- newddi.py
+++ newddi.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 
 
-def build_model(path, X_test):
+def build_model(path, dataset_number):
     csv_list = []
     file_counter = 0
     sentence_list = []
@@ -25,6 +25,8 @@ def build_model(path, X_test):
     tag_list = []
     sentence_all = []  #training data for word2vec
     EMBEDDING_DIM = 100
+    train_test = ''
+
 
     for filename in os.listdir(path):
         if not filename.endswith('.xml'): 
@@ -37,6 +39,8 @@ def build_model(path, X_test):
             if child.tag == 'Sentences':
                 sentences = child
                 break
+
+        train_test = 'train'
 
         for sentence_ind, sentence in enumerate(sentences.findall('Sentence')):
             sentenceText = sentence.find('SentenceText').text
@@ -58,19 +62,73 @@ def build_model(path, X_test):
                 pos = word_tag[1]
                 if word in tag_dict:
                     tag = tag_dict[word]
-                csv_list.append([file_counter, sentence_ind, word, pos, tag])
+                csv_list.append([file_counter, sentence_ind, word, pos, tag, train_test])
 
                 word_list.append(word)
                 tag_list.append(tag)
                 sentence_each.append(word)
-                temp_list.append((word, pos, tag))
+                temp_list.append((word, pos, tag, train_test))
             sentence_list.append(temp_list)
             sentence_all.append(sentence_each)
+
+    # loop for files within test folder
+    path = path + '/test'
+    file_counter = 0
+    for filename in os.listdir(path):
+        if not filename.endswith('.xml'): 
+            continue
+        fullname = os.path.join(path, filename)
+        tree = ET.parse(fullname)
+        file_counter += 1
+        root = tree.getroot()
+        for child in root:
+            if child.tag == 'Sentences':
+                sentences = child
+                break
+
+        train_test = 'test'
+
+
+        for sentence_ind, sentence in enumerate(sentences.findall('Sentence')):
+            sentenceText = sentence.find('SentenceText').text
+            # print(nltk.pos_tag(nltk.word_tokenize(sentenceText)))
+            tag_dict = {}
+            for mention in sentence.findall('Mention'):
+                for mentiounique_words in mention.attrib['str'].split('|'):
+                    for ind, mention_word in enumerate(mentiounique_words.split()):
+                        if not ind:
+                            tag_dict[mention_word] = 'B-' + mention.attrib['type']
+                        else:
+                            tag_dict[mention_word] = 'I-' + mention.attrib['type']
+
+            temp_list = []
+            sentence_each = []
+            for word_tag in nltk.pos_tag(nltk.word_tokenize(sentenceText)):
+                tag = 'O'
+                word = word_tag[0]
+                pos = word_tag[1]
+                if word in tag_dict:
+                    tag = tag_dict[word]
+                csv_list.append([file_counter, sentence_ind, word, pos, tag, train_test])
+
+                word_list.append(word)
+                tag_list.append(tag)
+                sentence_each.append(word)
+                temp_list.append((word, pos, tag, train_test))
+            sentence_list.append(temp_list)
+            sentence_all.append(sentence_each)
+
+        # for word in sentenceText.text:
+    with open('data.csv', 'a') as fileObj:
+        wr = csv.writer(fileObj, quoting=csv.QUOTE_ALL)
+        wr.writerow(['dataset_number', 'sentence_idx', 'word', 'pos', 'tag'])
+        for row in csv_list:
+            wr.writerow(row)
 
     # train word2vec model
     w2vmodel = Word2Vec(sentence_all, min_count=1)
     # summarize the loaded model
-    print(w2vmodel)
+    # print(w2vmodel)
 
     # words = list(set(word_list))
     words =[]
@@ -118,98 +176,14 @@ def build_model(path, X_test):
     model = Model(input, out)
     model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
     # model.summary()
-    history = model.fit(X, np.array(Y), batch_size=32, epochs=5, validation_split=0.1, verbose=1)
+    history = model.fit(X, np.array(Y), batch_size=32, epochs=30, validation_split=0.1, verbose=1)
 
     test_pred = model.predict(X_test, verbose=1)
     idx2tag = {i: w for w, i in tag2idx.items()}
-
+    #print(test_pred)
     return test_pred, idx2tag
 
-def get_pred_label(path):
-    path = path + '/test'
-    csv_list = []
-    file_counter = 0
-    sentence_list = []
-    word_list = []
-    tag_list = []
-    sentence_all = []  #training data for word2vec
-    EMBEDDING_DIM = 100
 
-    for filename in os.listdir(path):
-        if not filename.endswith('.xml'): 
-            continue
-        fullname = os.path.join(path, filename)
-        tree = ET.parse(fullname)
-        file_counter += 1
-        root = tree.getroot()
-        for child in root:
-            if child.tag == 'Sentences':
-                sentences = child
-                break
-
-        for sentence_ind, sentence in enumerate(sentences.findall('Sentence')):
-            sentenceText = sentence.find('SentenceText').text
-            # print(nltk.pos_tag(nltk.word_tokenize(sentenceText)))
-            tag_dict = {}
-            for mention in sentence.findall('Mention'):
-                for mentiounique_words in mention.attrib['str'].split('|'):
-                    for ind, mention_word in enumerate(mentiounique_words.split()):
-                        if not ind:
-                            tag_dict[mention_word] = 'B-' + mention.attrib['type']
-                        else:
-                            tag_dict[mention_word] = 'I-' + mention.attrib['type']
-
-            temp_list = []
-            sentence_each = []
-            for word_tag in nltk.pos_tag(nltk.word_tokenize(sentenceText)):
-                tag = 'O'
-                word = word_tag[0]
-                pos = word_tag[1]
-                if word in tag_dict:
-                    tag = tag_dict[word]
-                csv_list.append([file_counter, sentence_ind, word, pos, tag])
-
-                word_list.append(word)
-                tag_list.append(tag)
-                sentence_each.append(word)
-                temp_list.append((word, pos, tag))
-            sentence_list.append(temp_list)
-            sentence_all.append(sentence_each)
-
-    # words = list(set(word_list))
-    words =[]
-    for word_in_word_list in word_list:
-        if word_in_word_list not in words:
-            words.append(word_in_word_list)
-
-    words.append("ENDPAD")
-    unique_words = len(words)
-
-    # Embedding matrix for word2vec model
-    
-
-    tags = list(set(tag_list))
-    n_tags = len(tags)
-
-    word2idx = {w: i for i, w in enumerate(words)}
-    tag2idx = {t: i for i, t in enumerate(tags)}
-
-    # lenght of X is the no of sentences
-    X_test = [[word2idx[w[0]] for w in s] for s in sentence_list]
-
-    # pad all sentences with ENDPAD "O"
-    X_test = pad_sequences(maxlen=100, sequences=X_test, padding="post", value = unique_words - 1)
-
-    # labels for training and testing
-    Y_test = [[tag2idx[w[2]] for w in s] for s in sentence_list]
-
-    # pad labels whose indexes of all sentences equivalent of "O"
-    Y_test = pad_sequences(maxlen=100, sequences=Y_test, padding="post", value=tag2idx["O"])
-
-    # converted to one hot vector
-    Y_test = [to_categorical(i, num_classes=n_tags) for i in Y_test]
-
-    return X_test, Y_test
 
 def pred2label(pred, idx2tag):
     out = []
@@ -230,12 +204,7 @@ if __name__ == '__main__':
 
     for ever_dir in path_list:
         new_path = path + '/' +ever_dir
-        X_test, Y_test = get_pred_label(new_path)
-        test_pred, idx2tag = build_model(new_path, X_test)
-        pred_labels = pred2label(test_pred,idx2tag)
-        test_labels = pred2label(Y_test,idx2tag)
+        test_pred, idx2tag = build_model(new_path, ever_dir)
+        pred_labels = pred2label(test_pred, idx2tag)
+        test_labels = pred2label(Y_test, idx2tag)
         print(classification_report(test_labels, pred_labels))
-
-    
-
-        
